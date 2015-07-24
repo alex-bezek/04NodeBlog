40d0361e90a5a7402b4ea1fdccf4caa956ca43c7
Alex Bezek <alex.liam.bezek@gmail.com>
2015-07-23 22:58:53 -0500

    Can now post a new blog entry and has ckedit

--- app.js
+++ app.js
@@ -7,13 +7,12 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var mongo = require('mongodb');
-var db = require('monk')('localhost/nodeblog');
+var db =require('monk')('localhost/nodeblog');
 var multer = require('multer');
 var flash = require('connect-flash');
 
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+//var categories = require('./routes/categories');
 
 var app = express();
 
@@ -23,12 +22,18 @@ app.locals.moment = require('moment');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// Handle File Uploads & Multipart Data
+//app.use(multer({ dest: './public/images/uploads'}));
+
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+var routes = require('./routes/index');
+var posts = require('./routes/posts');
 
 // Express Session
 app.use(session({
@@ -55,7 +60,6 @@ app.use(expressValidator({
   }
 }));
 
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Connect-Flash
@@ -72,7 +76,8 @@ app.use(function(req,res,next){
 });
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/posts', posts);
+//app.use('/categories', categories);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/posts.js
@@ -0,0 +1,82 @@
+var express = require('express');
+var router = express.Router();
+var multer = require('multer');
+var mongo = require('mongodb');
+var db = require('monk')('localhost/nodeblog');
+
+//This was the way I got the multer to work, as opposed to how the tutorial said to do it
+var upload = multer({ dest: './public/images/uploads'})
+var cpUpload = upload.single('mainimage');
+
+router.get('/add', function(req, res, next){
+	var categories = db.get('categories');
+
+	categories.find({},{}, function(err, categories){
+		res.render('addpost',{
+			"title": "Add Post",
+			"categories": categories
+		});
+	});
+});
+
+router.post('/add', function(req, res, next){
+	console.log(req.body, req.file);
+
+	// Get Form Values
+	var title 		= req.body.title;
+	var category 	= req.body.category;
+	var body 		= req.body.body;
+	var author 		= req.body.author;
+	var date 		= new Date();
+
+	if(req.file){
+		var mainImageOriginalName 	= req.file.originalname;
+		var mainImageName 			= req.file.name;
+		var mainImageMime    		= req.file.mimetype;
+		var mainImagePath    		= req.file.path;
+		var mainImageExt    		= req.file.extension;
+		var mainImageSize    		= req.file.size;
+	} else {
+		var mainImageName = 'noimage.png';
+	}
+
+	// Form Validation
+	req.checkBody('title','Title field is required').notEmpty();
+	req.checkBody('body', 'Body field is required');
+
+	// Check Errors
+	var errors = req.validationErrors();
+
+	if(errors){
+		console.log('Found Form validation errors');
+		console.log(errors);
+		res.render('addpost',{
+			"errors": errors,
+			"title": title,
+			"body": body
+		});
+	} else {
+		console.log('No validation errors');
+		var posts = db.get('posts');
+
+		// Submit to DB
+		posts.insert({
+			"title": title,
+			"body": body,
+			"category": category,
+			"date": date,
+			"author": author,
+			"mainimage": mainImageName
+		}, function(err, post){
+			if(err){
+				res.send('There was an issue submitting the post');
+			} else {
+				req.flash('success','Post Submitted');
+				res.location('/');
+				res.redirect('/');
+			}
+		});
+	}
+});
+
+module.exports = router;
